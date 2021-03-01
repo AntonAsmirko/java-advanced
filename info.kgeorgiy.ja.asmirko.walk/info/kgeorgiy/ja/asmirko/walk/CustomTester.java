8f162b903ae68b6223256ca3abf20eb85a5fb592
AntonAsmirko <LoLkingXXX@gmail.com>
2021-03-01 18:17:44 +1000

    filewalker brocken

--- info.kgeorgiy.ja.asmirko.walk/info/kgeorgiy/ja/asmirko/walk/CustomTester.java
+++ info.kgeorgiy.ja.asmirko.walk/info/kgeorgiy/ja/asmirko/walk/CustomTester.java
@@ -3,7 +3,8 @@ package info.kgeorgiy.ja.asmirko.walk;
 import info.kgeorgiy.ja.asmirko.arrayset.ArraySet;
 import info.kgeorgiy.ja.asmirko.walk.Tests.CustomTest;
 import info.kgeorgiy.java.advanced.arrayset.SortedSetTest;
-import info.kgeorgiy.java.advanced.arrayset.Tester;
+import info.kgeorgiy.java.advanced.walk.Tester;
+import info.kgeorgiy.java.advanced.walk.WalkTest;
 
 import java.util.Comparator;
 import java.util.List;
@@ -14,7 +15,7 @@ public class CustomTester extends Tester {
         ArraySet<Integer> tmp = new ArraySet<Integer>(List.of(-1150527011, -907990455, 1479313065, -1858097033, -241732394, -1618266772, 1582437722, 1390312079), Comparator.reverseOrder());
         System.out.println(tmp.headSet(1582437723));
         new CustomTester()
-                .add("ArraySet", SortedSetTest.class)
+                .add("Walk", WalkTest.class)
                 .run(args);
     }
 }
--- info.kgeorgiy.ja.asmirko.walk/module-info.java
+++ info.kgeorgiy.ja.asmirko.walk/module-info.java
@@ -1,7 +1,9 @@
 module info.kgeorgiy.ja.asmirko.walk {
     requires info.kgeorgiy.java.advanced.arrayset;
+    requires info.kgeorgiy.java.advanced.walk;
 
     exports info.kgeorgiy.ja.asmirko.arrayset;
+    exports info.kgeorgiy.ja.asmirko.walk;
 
     opens info.kgeorgiy.ja.asmirko.walk to junit;
 }
