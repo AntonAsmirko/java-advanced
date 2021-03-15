c27247b9fc166b9dc0fed44fc05ccb3abcaeaa83
AntonAsmirko <LoLkingXXX@gmail.com>
2021-03-15 03:44:37 +0300

    NavigableArraySetAdded

---
+++ info.kgeorgiy.ja.asmirko.walk/info/kgeorgiy/ja/asmirko/arrayset/NavigableArraySet.java
@@ -0,0 +1,61 @@
+package info.kgeorgiy.ja.asmirko.arrayset;
+
+import java.util.Iterator;
+import java.util.NavigableSet;
+
+public class NavigableArraySet<T> extends ArraySet<T> implements NavigableSet<T> {
+    @Override
+    public T lower(T t) {
+        return null;
+    }
+
+    @Override
+    public T floor(T t) {
+        return null;
+    }
+
+    @Override
+    public T ceiling(T t) {
+        return null;
+    }
+
+    @Override
+    public T higher(T t) {
+        return null;
+    }
+
+    @Override
+    public T pollFirst() {
+        return null;
+    }
+
+    @Override
+    public T pollLast() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<T> descendingSet() {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<T> subSet(T fromElement, boolean fromInclusive, T toElement, boolean toInclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<T> headSet(T toElement, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<T> tailSet(T fromElement, boolean inclusive) {
+        return null;
+    }
+}
