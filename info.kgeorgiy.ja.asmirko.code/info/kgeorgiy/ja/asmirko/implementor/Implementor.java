dfed1acc361fef638f7d2475377967af4a65031c
AntonAsmirko <LoLkingXXX@gmail.com>
2021-04-06 05:53:16 +0300

    .

--- info.kgeorgiy.ja.asmirko.code/info/kgeorgiy/ja/asmirko/implementor/Implementor.java
+++ info.kgeorgiy.ja.asmirko.code/info/kgeorgiy/ja/asmirko/implementor/Implementor.java
@@ -2,18 +2,121 @@ package info.kgeorgiy.ja.asmirko.implementor;
 
 import info.kgeorgiy.java.advanced.implementor.Impler;
 import info.kgeorgiy.java.advanced.implementor.ImplerException;
+import info.kgeorgiy.java.advanced.implementor.JarImpler;
 
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.function.Function;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
 
-public class Implementor implements Impler {
+public class Implementor implements Impler, JarImpler {
+
+    public static void main(String[] args) {
+        if (args.length < 2 || args[0] == null || args[1] == null) {
+            System.out.println("Implementor should take two strings");
+            return;
+        }
+        Path resultFile = Path.of(args[1]);
+        Class<?> token;
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Implementor implementor = new Implementor();
+        try {
+            token = classLoader.loadClass(args[0]);
+            implementor.implementJar(token, resultFile);
+        } catch (ClassNotFoundException | ImplerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void implement(final Class<?> token, final Path root) throws ImplerException {
+        final int modifiers = token.getModifiers();
+        final Constructor<?>[] constructors = token.getDeclaredConstructors();
+        if (token.isPrimitive() || token.isEnum() ||
+                token == Enum.class || Modifier.isPrivate(modifiers) || Modifier.isFinal(modifiers)) {
+            throw new ImplerException("can't implement given token");
+        }
+        if (constructors.length != 0
+                && Arrays.stream(constructors).allMatch(c -> Modifier.isPrivate(c.getModifiers()))) {
+            throw new ImplerException("");
+
+        }
+        final String result = String.format("%s %n public class %sImpl %s %s {%n%n%s%n%s}",
+                token.getPackageName().isEmpty() ? "" : String.format("package %s;", token.getPackageName()),
+                token.getSimpleName(),
+                token.isInterface() ? "implements" : "extends",
+                token.getCanonicalName(),
+                constructorsCode(token),
+                methodsCode(token));
+
+        writeResult(token, root, result);
+    }
+
+    @Override
+    public void implementJar(Class<?> token, Path jarFile) throws ImplerException {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        try (var jOS = new JarOutputStream(Files.newOutputStream(jarFile))) {
+            String implementedPath = Path
+                    .of(token.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
+            if (compiler.run(null, null, null, "-cp", implementedPath,
+                    getPathToTargetFile(jarFile.getParent(), token, "java").toString()) != 0) {
+                throw new ImplerException("Can't compile generated class");
+            }
+            Manifest manifest = new Manifest();
+            manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+            jOS.putNextEntry(new ZipEntry(JarFile.MANIFEST_NAME));
+            manifest.write(jOS);
+            ZipEntry jar = new ZipEntry(getPathToTargetFile(jarFile.getParent(), token, "class").toString());
+            jOS.putNextEntry(jar);
+            Files.copy(getPathToTargetFile(jarFile.getParent(), token, "class"), jOS);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+            throw new ImplerException(e.getMessage());
+        } finally {
+//            try {
+//                CustomSimpleFileVisitor visitor = new CustomSimpleFileVisitor(jarFile, jarFile.getParent());
+//                Files.walkFileTree(jarFile.getParent(), visitor);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                throw new ImplerException(e.getMessage());
+//            }
+        }
+    }
+
+    private Path getPathToTargetFile(Path jarFile, Class<?> token, String fileExtension) {
+        return Paths.get(jarFile.toString(), token.getPackageName().replace(".", File.separator),
+                token.getSimpleName() + "Impl." + fileExtension);
+    }
+
+    private void writeResult(Class<?> token, Path root, String result) throws ImplerException {
+        Path pathToFile = root.resolve(token.getPackageName().replace(".", File.separator));
+        Path file = pathToFile.resolve(String.format("%sImpl.java", token.getSimpleName()));
+        try {
+            Files.createDirectories(pathToFile);
+            if (Files.notExists(file)) {
+                Files.createFile(file);
+            }
+            try (BufferedWriter bw = Files.newBufferedWriter(file)) {
+                bw.write(result);
+            }
+        } catch (IOException e) {
+            throw new ImplerException(String.format("Internal error: %s", e.getMessage()));
+        }
+    }
 
     private Stream<MethodWrapper> getAllMethods(Class<?> token) {
         Stream<MethodWrapper> processedMethods = Arrays.stream(token.getMethods()).map(MethodWrapper::new);
@@ -36,7 +139,6 @@ public class Implementor implements Impler {
                 joinExceptions(executable.getExceptionTypes()), b);
     }
 
-
     private <T> String streamMapAndJoin(T[] arr, Function<T, String> fn) {
         return Arrays.stream(arr).map(fn).collect(Collectors.joining(", "));
     }
@@ -58,47 +160,22 @@ public class Implementor implements Impler {
         return clazz != void.class ? clazz == boolean.class ? "false" : !clazz.isPrimitive() ? "null" : "0" : "";
     }
 
-    @Override
-    public void implement(final Class<?> token, final Path root) throws ImplerException {
-        final int modifiers = token.getModifiers();
-        final Constructor<?>[] constructors = token.getDeclaredConstructors();
-        if (token.isPrimitive() || token.isEnum() ||
-                token == Enum.class || Modifier.isPrivate(modifiers) || Modifier.isFinal(modifiers)) {
-            throw new ImplerException("can't implement given token");
-        }
-        if (constructors.length != 0
-                && Arrays.stream(constructors).allMatch(c -> Modifier.isPrivate(c.getModifiers()))) {
-            throw new ImplerException("");
-        }
-        final String result = String.format("%s %n public class %sImpl %s %s {%n%n%s%n%s}",
-                token.getPackageName().isEmpty() ? "" : String.format("package %s;", token.getPackageName()),
-                token.getSimpleName(),
-                token.isInterface() ? "implements" : "extends",
-                token.getCanonicalName(),
-                mapAndJoin(Arrays.stream(token.getDeclaredConstructors())
-                                .filter(c -> !Modifier.isPrivate(c.getModifiers())),
-                        m -> mm(m, c -> c.getDeclaringClass().getSimpleName(),
-                                "public %s%sImpl(%s) %s {%n super(%s);%n}%n",
-                                streamMapAndJoin(m.getParameters(), Parameter::getName))),
-                mapAndJoin(getAllMethods(token)
-                                .map(MethodWrapper::getMethod),
-                        m -> mm(m, Method::getName,
-                                "public %s %s(%s) %s {%n return %s;%n}%n",
-                                getDefaultValue(m.getReturnType()),
-                                Modifier.isStatic(m.getModifiers()) ? "static " : "",
-                                m.getReturnType().getCanonicalName())));
+    private String constructorsCode(Class<?> token) {
+        return mapAndJoin(Arrays.stream(token.getDeclaredConstructors())
+                        .filter(c -> !Modifier.isPrivate(c.getModifiers())),
+                m -> mm(m, c -> c.getDeclaringClass().getSimpleName(),
+                        "public %s%sImpl(%s) %s {%n super(%s);%n}%n",
+                        streamMapAndJoin(m.getParameters(), Parameter::getName)));
+    }
 
-        Path pathToFile = root.resolve(token.getPackageName().replace(".", "/"));
-        Path file = pathToFile.resolve(String.format("%sImpl.java", token.getSimpleName()));
-        try {
-            Files.createDirectories(pathToFile);
-            Files.createFile(file);
-            try (BufferedWriter bw = Files.newBufferedWriter(file)) {
-                bw.write(result);
-            }
-        } catch (IOException e) {
-            throw new ImplerException(String.format("Internal error: %s", e.getMessage()));
-        }
+    private String methodsCode(Class<?> token) {
+        return mapAndJoin(getAllMethods(token)
+                        .map(MethodWrapper::getMethod),
+                m -> mm(m, Method::getName,
+                        "public %s %s(%s) %s {%n return %s;%n}%n",
+                        getDefaultValue(m.getReturnType()),
+                        Modifier.isStatic(m.getModifiers()) ? "static " : "",
+                        m.getReturnType().getCanonicalName()));
     }
 
     private static class MethodWrapper {
@@ -134,6 +211,33 @@ public class Implementor implements Impler {
 
         public Method getMethod() {
             return method;
+        }
+    }
+
+    private static class CustomSimpleFileVisitor extends SimpleFileVisitor<Path> {
+
+        final private HashSet<Path> excludePaths;
+
+        public CustomSimpleFileVisitor(Path... excludePaths) {
+            this.excludePaths = new HashSet<>(Arrays.asList(excludePaths));
+        }
+
+        @Override
+        public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+            deleteIfAbsent(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(final Path dir, final IOException exc) throws IOException {
+            deleteIfAbsent(dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        private void deleteIfAbsent(Path path) throws IOException {
+            if (!excludePaths.contains(path)) {
+                Files.delete(path);
+            }
         }
     }
 }
---
+++ info/kgeorgiy/java/advanced/implementor/basic/interfaces/standard/RandomAccessImpl.java
@@ -0,0 +1,5 @@
+package info.kgeorgiy.java.advanced.implementor.basic.interfaces.standard; 
+ public class RandomAccessImpl implements info.kgeorgiy.java.advanced.implementor.basic.interfaces.standard.RandomAccess {
+
+
+}
